0c08a83dffbe6049028e25558ebcc88be2f729b8
Sicheng Zhu <zsc@uw.edu>
2017-08-02 17:32:57 -0700

    single.php

--- single.php
+++ single.php
@@ -1,17 +1,20 @@
 <?php get_header(); ?>
 
     <!-- Begin Content -->
-    <div id="content">
+    <div class="post" style="width:940px;float:center;margin: 0 auto;">
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); // start the loop ?>
-    <article id="post-<?php the_ID(); ?>" class="post">
-    <h2><?php the_title(); // the posting title ?></h2>
-    <small>Posted on <?php the_time('F j, Y'); // the time ?> by <?php the_author(); // the author name ?> in <?php the_category(', '); // the category ?></small>
-    <?php the_post_thumbnail( 'large' ); // the featured image ?>
-    <?php the_content(''); // posting written content ?>
-    </article>
-    <?php endwhile; endif; // end the loop ?>
-    <small>single.php</small>
+        <div class="leftColumn" style="width:600px;float:left;">
+        
+            <?php the_post_thumbnail( 'large' ); // the featured image ?>
+        
+            <div class="postContent" style="width:540px;margin:0 auto;">
+            <h3><?php the_title(); // the posting title ?></h3>
+            <?php the_content(''); // posting written content ?>        
+            </div>    
+        </div>
+    <?php endwhile; endif; // end the loop ?>    
     </div>
+    <small>single.php</small>
     <!-- End Content -->
     
 <?php get_sidebar(); ?>
